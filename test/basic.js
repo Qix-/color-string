5e3e6a0edeaa2e7adf7324a8c6b5de1e93d56661
halvves <hi@halvves.com>
2017-02-22 08:31:39 -0600

    add tests for 0 alpha hex string generation

--- test/basic.js
+++ test/basic.js
@@ -101,6 +101,8 @@ assert.equal(string.to.hex([255, 10, 35, 1]), '#FF0A23');
 assert.equal(string.to.hex([255, 10, 35], 1), '#FF0A23');
 assert.equal(string.to.hex([255, 10, 35, 0.3]), '#FF0A234D');
 assert.equal(string.to.hex([255, 10, 35], 0.3), '#FF0A234D');
+assert.equal(string.to.hex([255, 10, 35, 0]), '#FF0A2300');
+assert.equal(string.to.hex([255, 10, 35], 0), '#FF0A2300');
 
 assert.equal(string.to.rgb([255, 10, 35]), 'rgb(255, 10, 35)');
 assert.equal(string.to.rgb([255, 10, 35, 0.3]), 'rgba(255, 10, 35, 0.3)');
