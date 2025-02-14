947a837ffd3e50983c954882f6a8bec8bd9817c0
Christian Rackerseder <git@echooff.de>
2025-02-14 09:16:39 +0100

    Change typings to always return null instead of undefined

--- index.d.ts
+++ index.d.ts
@@ -2,20 +2,20 @@ export type Model = 'rgb' | 'hsl' | 'hwb';
 
 export type ColorString = {
 	get: {
-		(color: string): {model: Model; value: number[]} | undefined;
-		rgb: (color: string) => number[] | undefined;
-		hsl: (color: string) => number[] | undefined;
-		hwb: (color: string) => number[] | undefined;
+		(color: string): {model: Model; value: number[]} | null;
+		rgb: (color: string) => number[] | null;
+		hsl: (color: string) => number[] | null;
+		hwb: (color: string) => number[] | null;
 	};
 	to: {
-		hex: (r: number, g: number, b: number, a?: number) => string | undefined;
+		hex: (r: number, g: number, b: number, a?: number) => string | null;
 		rgb: {
-			(r: number, g: number, b: number, a?: number): string | undefined;
-			percent: (r: number, g: number, b: number, a?: number) => string | undefined;
+			(r: number, g: number, b: number, a?: number): string | null;
+			percent: (r: number, g: number, b: number, a?: number) => string | null;
 		};
-		keyword: (r: number, g: number, b: number, a?: number) => string | undefined;
-		hsl: (h: number, s: number, l: number, a?: number) => string | undefined;
-		hwb: (h: number, w: number, b: number, a?: number) => string | undefined;
+		keyword: (r: number, g: number, b: number, a?: number) => string | null;
+		hsl: (h: number, s: number, l: number, a?: number) => string | null;
+		hwb: (h: number, w: number, b: number, a?: number) => string | null;
 	};
 };
 
